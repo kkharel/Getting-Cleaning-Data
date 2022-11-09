8e3f43b13163c9ab42dc600a26a68973f36b173d
kkharel <kkharel@uci.edu>
2022-11-08 20:21:42 -0800

    getting and reading from different sources final

--- Getting Data.R
+++ Getting Data.R
@@ -274,3 +274,138 @@ affyMisSmall = fetch(query, n=10); dbClearResult(query)
 dim(affyMisSmall)
 
 dbDisconnect(hg19)
+
+# Reading from HDF5 
+  # for storing large datasets. heirarchial data format
+
+# source("http://bioconductor.org/biocLite.R")
+biocLite("rhdf5")
+# install.packages("BiocManager")
+# BiocManager::install("biocLite")
+
+library(rhdf5)
+
+created = h5createFile("example.h5")
+created
+
+# creating groups
+created = h5createGroup("example.h5", "foo")
+created = h5createGroup("example.h5", "baa")
+
+# create subgroup
+created = h5createGroup("example.h5", "foo/foobaa")
+
+h5ls("example.h5")
+
+# Writing to Groups
+
+A = matrix(1:10, nr = 5, nc = 2)
+h5write(A, "example.h5", "foo/A")
+
+# multi-dimensional array
+
+B = array(seq(0.1, 2.0, by =0.1),dim = c(5,2,2))
+attr(B, "scale") = "liter"
+h5write(B, "example.h5", "foo/foobaa/B")
+h5ls("example.h5")
+
+# Writing a data set
+
+df = data.frame(1L:5L, sql(0,1, length.out = 5),
+                c("ab", "cde", "fghi", "a", "s"),
+                stringsAsFactors = FALSE)
+h5write(df, "example.h5", "df")
+h5ls("example.h5")
+
+# Reading Data
+
+readA = h5read("example.h5", "foo/A")
+readB = h5read("example.h5", "foo/foobaa/B")
+readdf = h5read("example.h5", "df")
+readA
+
+# Writing and reading chunks
+
+h5write(c(12,13,14), "example.h5", "foo/A", index=list(1:3,1))
+h5read("example.h5", "foo/A")
+
+
+# Reading from the WEB
+
+# Getting data off of webpages with readLines()
+
+con = url("http://scholar.google.com/citations?user=HI-I6C0AAAAJ&hl=en")
+htmlCode = readLines(con)
+close(con)
+htmlCode
+
+# Then parsing with XML package
+
+library(XML)
+
+url = "http://scholar.google.com/citations?user=HI-I6C0AAAAJ&hl=en"
+
+html = htmlTreeParse(url, useInternalNodes = T)
+
+xpathSApply(html, "//title", xmlValue)
+
+
+# Getting data with GET request
+
+library(httr)
+
+html2 = GET(url)
+content2 = content(html2, as="text")
+parsedHtml = htmlParse(content2,asText = TRUE)
+xpathSApply(parsedHtml, "//title", xmlValue)
+
+# When authentication is required then,
+
+html3 = GET(url, 
+            authenticate("username", "password"))
+
+html3
+
+names(html3)
+
+# Use handles
+
+google = handle("http://google.com")
+
+pg1 = GET(handle=google, path="/")
+pg2 = GET(handle=google, path="search")
+
+# Getting data from API's
+
+# Accessibng Twitter from R
+
+myapp = oauth_app("twitter", key = "consumerkey", secret="consumersecret")
+
+sig = sign_oauth1.0(myapp, token = "yourtoken", token_secret = "tokensecret")
+
+homeTL = GET("https://api.twitter.com/1.1/statuses/home_timeline.json", sig)
+
+# Converting the json object
+
+json1 = content(homeTL)
+json2 = jsonlite::fromJSON(toJSON(json1)) # creating dataframe from json object
+json2[1,1:4] # first row and first 4 columns
+
+# Read twitter Documentation
+
+# httr allows GET, POST, PUT, DELETE requests if we are authorized. httr works well with FB, Google, Github etc..
+
+
+# Foreign package loads data from Minitab, SAS, SPSS, Stata etc
+
+# read.arfF(weka), read.dta(stata), read.mtp(minitab), read.octave(octave), read.spss(spss), read.xport(sas)
+
+# RPostresSQL provides a DBI-compliant database connection from R
+
+# Reading images: jpeg, readbitmap, png, EBImage(bioconductor)
+
+# Reading GIS data: rdgal, rgeos, raster
+
+# Reading music data: tuneR, seewave
+
+
