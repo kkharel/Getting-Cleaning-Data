a014514201cb311b2546b49d9a6f1d7649504623
kkharel <kkharel@uci.edu>
2022-12-11 19:14:42 -0800

    final merging

--- OrganizingMergingManaging.R
+++ OrganizingMergingManaging.R
@@ -308,4 +308,43 @@ chicago %>%
 
 # Merging Data
 
+if(!file.exists)("./data"){{dir.create("./data")}}
 
+fileUrl1 = "https://s3.amazonaws.com/csvpastebin/uploads/e70e9c289adc4b87c900fdf69093f996/reviews.csv"
+fileUrl2 = "https://s3.amazonaws.com/csvpastebin/uploads/0863fd2414355555be0260f46dbe937b/solutions.csv"
+download.file(fileUrl1, destfile="./data/reviews.csv", method = "curl")
+download.file(fileUrl2, destfile="./data/solutions.csv", method = "curl")
+reviews = read.csv("./data/reviews.csv")
+solutions = read.csv("./data/solutions.csv")
+head(reviews,2)
+head(solutions,2)
+
+
+# Merge()
+# important parameters: x,y,by.x,by.y,all
+names(reviews)
+names(solutions)
+
+mergedData = merge(reviews,solutions, by.x="solution_id", by.y="id", all=TRUE)
+head(mergedData)
+
+# Defauly - merge all common column names
+
+intersect(names(solutions), names(reviews))
+
+mergedData2 = merge(reviews, solutions, all=TRUE)
+head(mergedData2)
+
+# Using join in the plyr package
+#faster, but less full featured - defaults to left join
+library(plyr)
+df1 = data.frame(id=sample(1:10), x = rnorm(10))
+df2 = data.frame(id=sample(1:10), y = rnorm(10))
+arrange(join(df1,df2),id)
+
+# if multiple data frames then
+df1 = data.frame(id=sample(1:10), x = rnorm(10))
+df2 = data.frame(id=sample(1:10), y = rnorm(10))
+df3 = data.frame(id=sample(1:10), z = rnorm(10))
+df_List = list(df1,df2,df3)
+join_all(df_List)
